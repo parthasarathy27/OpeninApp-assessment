783166cbd05307b29654b11e0b664221c122f482
Parthasarathy G <83574852+parthasarathy27@users.noreply.github.com>
2023-10-16 19:06:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import { useEffect } from "react";
+import {
+  Routes,
+  Route,
+  useNavigationType,
+  useLocation,
+} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Dashboard1 from "./pages/Dashboard1";
+import Dashboard2 from "./pages/Dashboard2";
+import Dashboard3 from "./pages/Dashboard3";
+
+function App() {
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/dashboard":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/dashboard2":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/dashboard1":
+        title = "";
+        metaDescription = "";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/dashboard" element={<Dashboard1 />} />
+      <Route path="/dashboard2" element={<Dashboard2 />} />
+      <Route path="/dashboard1" element={<Dashboard3 />} />
+    </Routes>
+  );
+}
+export default App;
